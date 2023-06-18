77090d89cce3a67037464a14afd3185fbbfd77a3
kylelin1998 <email@kylelin1998.com>
2023-06-18 15:43:00 +0800

    Change docker-push.sh

--- docker-push.sh
+++ docker-push.sh
@@ -4,4 +4,4 @@ sudo apt install -y qemu-user-static binfmt-support
 docker buildx create --use
 docker buildx ls
 
-docker buildx build --platform linux/amd64, linux/386, linux/arm64 -t kylelin1998/rss-tg-bot . --push
+docker buildx build --platform linux/amd64,linux/386,linux/arm64 -t kylelin1998/rss-tg-bot . --push
