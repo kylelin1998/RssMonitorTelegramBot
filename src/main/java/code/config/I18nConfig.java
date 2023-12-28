0e0da9b03d7fb5dd733cf5c48ce892d2a4b0ba7f
kylelin1998 <email@kylelin1998.com>
2023-12-28 18:44:21 +0800

    Fixed reading encode bug

--- src/main/java/code/config/I18nConfig.java
+++ src/main/java/code/config/I18nConfig.java
@@ -2,18 +2,38 @@ package code.config;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class I18nConfig {
 
     private static Map<String, Map<String, String>> cacheMap = new LinkedHashMap<>();
 
     static {
+        ResourceBundle.Control control = new ResourceBundle.Control() {
+            @Override
+            public ResourceBundle newBundle(String baseName, java.util.Locale locale, String format, ClassLoader loader, boolean reload) throws IOException {
+                String resourceName = toResourceName(toBundleName(baseName, locale), "properties");
+                InputStream inputStream = null;
+                try {
+                    inputStream = loader.getResourceAsStream(resourceName);
+                    if (inputStream != null) {
+                        return new PropertyResourceBundle(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                    }
+                } finally {
+                    if (inputStream != null) {
+                        inputStream.close();
+                    }
+                }
+                return null;
+            }
+        };
         for (I18nLocaleEnum value : I18nLocaleEnum.values()) {
-            ResourceBundle bundle = ResourceBundle.getBundle("i18n/i18n", value.getLocale());
+            ResourceBundle bundle = ResourceBundle.getBundle("i18n/i18n", value.getLocale(), control);
+
             HashMap<String, String> hashMap = new HashMap<>();
             for (String s : bundle.keySet()) {
                 hashMap.put(s, bundle.getString(s));
